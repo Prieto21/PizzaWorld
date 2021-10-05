87a435feff82936a4db0f51bea8d3ecd0d2eb92a
Prieto21 <seraleprimol@gmail.com>
2021-10-04 19:11:46 -0500

    Primer Avance

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,73 @@
-<center>
-<h1>Login</h1> <hr>
-<a href="http://localhost:8000/PizzaWorld/User">
-    <button>
-        Entrar
-    </button>
-</a> &nbsp;&nbsp;&nbsp;
-<a href="http://localhost:8000/PizzaWorld/Admin">
-    <button>
-        Entrar Admin
-    </button>
-</a> &nbsp;&nbsp;&nbsp;
+<!doctype html>
+<html lang="en">
 
-<a href="http://localhost:8000/PizzaWorld/registro">
-    <button>
-        Registro
-    </button>
-</a>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-</center>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>World of pizza</title>
+</head>
+
+<body class="body">
+    <br><br><br><br><br><br><br>
+    <center>
+       
+        <div class="contenedorlogin">
+            
+            <form >
+            <h1>Login</h1>
+            <hr><br>
+                <!-- USUARIO -->
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
+                    </span>
+                    <input type="text"  class="form-control" placeholder="Usuario" aria-label="Username" aria-describedby="basic-addon1" required>
+                </div>
+                <br>
+                <!-- CONTRASEÑA -->
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+                        </svg>
+                    </span>
+                    <input type="password" class="form-control" placeholder="Contraseña" aria-label="Username" aria-describedby="basic-addon1" required>
+                </div>
+                <br>
+                <a href="http://52.14.138.109:81/PizzaWorld/User">
+                <!-- <a href="http://localhost:8000/PizzaWorld/User"> -->
+                <button type="button" class="btn btn1 ">Entrar</button>
+                </a>
+                
+            </form>
+            <a class="link_registro" href="http://52.14.138.109:81/PizzaWorld/registro">
+            <!-- <a class="link_registro" href="http://localhost:8000/PizzaWorld/registro"> -->
+                ¿No Tienes Cuenta?
+            </a>
+        </div>
+        
+
+    </center>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,9 +1,124 @@
-<center>
-<h1>Registro</h1>
-<hr>
-<a href="http://localhost:8000/PizzaWorld/login">
-    <button>
-        Cancelar
-    </button>
-</a>
-</center>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>World of pizza</title>
+</head>
+
+<body class="body">
+    <br><br><br><br><br><br>
+    <center>
+
+        <div class="contenedorlogin">
+
+            <form>
+                <h1>Registro</h1>
+                <hr><br>
+                <!-- NOMBRE COMPLETO -->
+                <div class="row g-2">
+                    <div class="col-md">
+                        <div class="input-group mb-3">
+
+                            <input type="text"  class="form-control" placeholder="Nombres" aria-label="Username" aria-describedby="basic-addon1" required>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="input-group mb-3">
+
+                            <input type="text" required class="form-control" placeholder="Apellidos" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- USUARIO (imput de dos en dos) -->
+                <div class="row g-2">
+                    <div class="col-md">
+                        <div class="input-group mb-3">
+
+                            <input type="text" required class="form-control" placeholder="Usuario" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="input-group mb-3">
+
+                            <input type="text" required class="form-control" placeholder="Contraseña" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Email -->
+
+                <div class="input-group mb-3">
+
+                    <input type="text" required class="form-control" placeholder="Correo Electronico" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+
+                <!-- Ultimos datos -->
+                <div class="row g-2">
+                    <div class="col-md">
+                        <div class="input-group mb-3">
+
+
+                            <label for="formFileSm" class="form-label">Foto De Perfil</label> &nbsp
+                            <input required class="form-control form-control-sm" id="formFileSm" type="file">
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="input-group mb-3">
+
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Sexo</option>
+                                <option value="1">Hombre</option>
+                                <option value="2">Mujer</option>
+                                <option value="3">Otro</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="contenedor_terminos">
+                <p>Acepto Los Terminos</p>
+                <div class="form-check form-switch">
+                    
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked required>
+                    
+                </div>
+                </div>
+                
+                
+                <a href="http://52.14.138.109:81/PizzaWorld/User">
+                <!-- <a href="http://localhost:8000/PizzaWorld/User"> -->
+                    <button type="button" class="btn btn1 ">Crear</button>
+                </a>
+
+            </form>
+            <a class="link_registro" href="http://52.14.138.109:81/PizzaWorld/login">
+            <!-- <a class="link_registro" href="http://localhost:8000/PizzaWorld/login"> -->
+                Atras
+            </a>
+        </div>
+
+
+    </center>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
